b8e877d98db505579c42fa88afe96566a814ea97
fion chen <wakinglife_615@yahoo.com>
2018-04-18 21:47:19 +0800

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  devise_for :users
+  # devise_for :users
 
-     root "articles#index"
+     root "posts#index"
 
-     resources :articles, only: [:index, :create, :show, :edit, :update, :destroy] do
-       resources :comments, only: [:index,:create, :edit, :update, :destroy]
+     resources :psots, only: [:index, :create, :show, :edit, :update, :destroy] do
+       resources :comments, only: [:index, :create, :edit, :update, :destroy]
        member do
          post :like
          post :unlike
@@ -21,26 +21,28 @@ Rails.application.routes.draw do
 
       resources :users, only: [:index, :show, :edit, :update, :destroy] do
         member do
-          # get :articles
-          # get :likes
-          # get :comments
-          #
-          # get :friends
-          # get :inversefriends
+
           get :friend_list
 
         end
       end
 
-      resources :friendships, only: [:create, :destroy]
-      resources :likes, only: [:create, :destroy]
-      resources :comments, only: [:create, :edit, :destroy]
+      resources :friendships, only: [:create, :destroy] do
+        member do
+          post :confirm
+          delete :reject
+
+        end
+     end
+
+
       resources :categories, only: :show
+      resources :views, only: [:index, :show]
 
 
      namespace :admin do
-       resources :forums, only: [:index, :destroy]
-       resources :categoties, only: [:index, :create, :show, :edit, :update, :destroy]
+       resources :posts, only: [:index, :destroy]
+       resources :categories, only: [:index, :create, :show, :edit, :update, :destroy]
        resources :users, only: [:index]
        root "articles#index"
      end
