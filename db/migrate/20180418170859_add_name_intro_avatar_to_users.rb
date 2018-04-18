8f9bfc9987b5e13bf892b3f367a5e53a3622ca50
fion chen <wakinglife_615@yahoo.com>
2018-04-19 01:18:34 +0800

    add name, intro, avatar column to user table

---
+++ db/migrate/20180418170859_add_name_intro_avatar_to_users.rb
@@ -0,0 +1,8 @@
+class AddNameIntroAvatarToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :intro, :text
+    add_column :users, :avatar, :string
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418161429) do
+ActiveRecord::Schema.define(version: 20180418170859) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 20180418161429) do
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "intro"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
