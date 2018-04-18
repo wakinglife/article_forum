2ca2acb1cbc6d896ebe648cb4cac9b3f5b44f9f3
fion chen <wakinglife_615@yahoo.com>
2018-04-19 00:20:28 +0800

    create user model using devise

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+
+  def index
+    # @users = User.order(created_at: :desc).limit(10)
+    @posts = Post.all
+    # @posts = Post.new
+    # @posts = Post.page(params[:page]).per(20).order(created_at: :desc)
+
+  end
+
+
+
+  def create
+
+    @post = current_user.posts.build(post_params)
+       if @post.save
+         flash[:notice] = 'post was successfully created'
+         redirect_to posts_path
+       else
+         flash[:alert] = @post.errors.full_messages.to_sentence
+         @posts = Post.page(params[:page]).per(20).order(created_at: :desc)
+         render :index
+       end
+  end
+
+  private
+
+def set_post
+   @post = Post.find(params[:id])
+end
+
+
+def post_params
+  params.require(:post).permit(:content)
+end
+
+end
