b5444afae5a61f837286c2e85a6d4ca9c5d66266
fion chen <wakinglife_615@yahoo.com>
2018-04-20 22:40:49 +0800

    add create, update and destory to  category controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,4 +6,42 @@ class Admin::CategoriesController < Admin::BaseController
     @category = Category.new
 
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "category was successfully created"
+      redirect_to admin_categories_path
+    else
+      @categories = Category.page(params[:page]).per(15)
+      render :index
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to admin_categories_path
+      flash[:notice] = "category was successfully updated"
+    else
+      @categories = Category.page(params[:page]).per(15)
+      render :index
+    end
+  end
+
+  def destroy
+    @category.destroy
+    flash[:alert] = "category was successfully deleted"
+    redirect_to admin_categories_path
+  end
+
+  private
+
+  def set_category
+  @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
