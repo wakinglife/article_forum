ce8f46f9f0aa185e21989004f1d49f38fd8d3201
fion chen <wakinglife_615@yahoo.com>
2018-04-24 03:46:05 +0800

    create like model and add relation between like and use,post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423180708) do
+ActiveRecord::Schema.define(version: 20180423193714) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20180423180708) do
     t.text "comment"
     t.integer "post_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
