57a6dec4beace30a41e9886bc393e86de75b0f1e
fion chen <wakinglife_615@yahoo.com>
2018-04-26 01:59:10 +0800

    add friendship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180425170744) do
+ActiveRecord::Schema.define(version: 20180425175034) do
 
   create_table "categories", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20180425170744) do
     t.text "comment"
     t.integer "post_id"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
