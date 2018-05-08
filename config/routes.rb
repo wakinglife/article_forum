b9fe9fb42b3416ae744046837214832375fafa77
fion chen <wakinglife_615@yahoo.com>
2018-05-08 21:24:19 +0800

    update login path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { registrations: "users/registrations" }
+  devise_for :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-     root "posts#index"
+  root "posts#index"
 
      resources :posts do
        resources :comments, only: [:create, :edit, :update, :destroy]
